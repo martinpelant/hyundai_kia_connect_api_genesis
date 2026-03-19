f2e121f72832c25de10335754cb28264191fdc52
Martin Pelant <pelant@localhost.com>
2026-03-19 00:49:44 +0000

    chore: bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/Hyundai-Kia-Connect/hyundai_kia_connect_api",
-    version="1.0.0",
+    version="1.0.1",
     zip_safe=False,
 )
